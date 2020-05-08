ad85f0ed9d9e8bd7cb548f46b26f0bff3944b6c7
Scott Huang <sunnyhsjy@gmail.com>
2020-05-07 19:39:03 -0700

    responsive design

--- src/components/KeyWord/KeyWordCard.js
+++ src/components/KeyWord/KeyWordCard.js
@@ -6,19 +6,29 @@ import Typography from "@material-ui/core/Typography";
 import ts_profile from "assets/img/taylor-profile.jpg";
 import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody";
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme) => ({
+let fontTheme = createMuiTheme();
+fontTheme = responsiveFontSizes(fontTheme);
+
+const useStyles = makeStyles(theme => ({
   root: {
-    height: 245,
-    width: 200,
+    height: "auto",
+    width: "95%",
     background: "#1C1433",
-    color: "#CACACA",
+    color: "#CACACA"
   },
   media: {
     height: 0,
-    marginTop:15,
-    width:140,
-    paddingTop: '100%', // 16:9
+    marginTop: "8%",
+    width: "97%",
+    paddingTop: "100%" // 16:9
+  },
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    fontSize: 4,
   },
 }));
 
@@ -26,22 +36,24 @@ export default function KeyWordCard() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
-//   const handleExpandClick = () => {
-//     setExpanded(!expanded);
-//   };
+  //   const handleExpandClick = () => {
+  //     setExpanded(!expanded);
+  //   };
 
   return (
     <Card className={classes.root}>
       <CardBody>
         <CardMedia className={classes.media} image={ts_profile} />
-      <CardContent>
-        <Typography variant="body1" align="center">
-            Taylor Swift
-        </Typography>
-        <Typography variant="body2" align="center">
-            CMBYN
-        </Typography>
-      </CardContent>
+        <CardContent>
+          <ThemeProvider theme={fontTheme}>
+            <Typography variant="body1" align="center">
+              Taylor Swift
+            </Typography>
+            <Typography variant="body2" align="center">
+              CMBYN
+            </Typography>
+          </ThemeProvider>
+        </CardContent>
       </CardBody>
     </Card>
   );
